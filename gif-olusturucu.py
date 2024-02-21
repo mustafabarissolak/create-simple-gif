5c7aebf7799f8d04ff9933a636739cf81d32edc6
Barış Solak <mustafabarissolak@gmail.com>
2024-02-21 18:06:11 +0300

    Update gif-olusturucu.py

--- gif-olusturucu.py
+++ gif-olusturucu.py
@@ -1,6 +1,5 @@
-# pip install pillow
-
 from PIL import Image, ImageDraw
+# pip install pillow
 
 width, height = 300, 300
 background_color = (255, 255, 255) 
@@ -10,15 +9,12 @@ num_frames = 30
 for i in range(num_frames):
     image = Image.new("RGB", (width, height), background_color)
     gradient_color = (i * 10, 0, 255 - i * 10)
-    
     draw = ImageDraw.Draw(image)
     draw.rectangle([0, 0, width, height], fill=background_color)
-    
     radius = 20
     x_pos = (width - 2 * radius) * i / (num_frames - 1)
     y_pos = height / 2
     circle_box = [x_pos, y_pos - radius, x_pos + 2 * radius, y_pos + radius]
-    
     draw.ellipse(circle_box, fill=gradient_color, outline=(0, 0, 0))
     frames.append(image)
 
